f42f53966c3bf4d61b45eaafdcaf09782a7cd9a4
Abel Martínez <abel.martinez@wizeline.com>
2019-11-05 15:01:50 -0800

    Initial Commit

---
+++ CalendarComponent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarComponent
+//
+//  Created by Abel Martinez  on 05/11/19.
+//  Copyright © 2019 a7x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
